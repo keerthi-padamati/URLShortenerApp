7238c2d95273d11519b81f5909405181655e1ef3
keerthi-padamati <keerthipadamati16@gmail.com>
2025-10-23 19:39:00 +0530

    Reformat imports and initialize variables

--- URLShortenerGUI.java
+++ URLShortenerGUI.java
@@ -4,5 +4,11 @@ import java.awt.event.*;
 import java.net.URI;
 import java.util.*;
 
-class URLShortnerGUI{
+class URLShortnerGUI
+{
+   private Map<String, String> shortToUrl = new HashMap<>();
+    private static final String CHAR_POOL = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int CODE_LENGTH = 6;
+    private Random random = new Random();
   
+
