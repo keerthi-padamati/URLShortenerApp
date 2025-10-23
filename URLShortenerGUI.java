21e086545ac0c41a99890b4729988d1c21187724
keerthi-padamati <keerthipadamati16@gmail.com>
2025-10-23 19:39:52 +0530

    Fix imports and update CODE_LENGTH comment
    Added missing imports and adjusted comment for CODE_LENGTH.

--- URLShortenerGUI.java
+++ URLShortenerGUI.java
@@ -8,7 +8,8 @@ class URLShortnerGUI
 {
    private Map<String, String> shortToUrl = new HashMap<>();
     private static final String CHAR_POOL = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final int CODE_LENGTH = 6;
+    private static final int CODE_LENGTH = 6; // you replace the output url size heres
     private Random random = new Random();
   
+
 
